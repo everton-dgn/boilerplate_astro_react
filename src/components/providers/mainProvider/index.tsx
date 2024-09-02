d9ee66f53d872c74dffa6234d03af6b62a7d9c74
Éverton Toffanetto <evertondgn@hotmail.com>
2024-09-02 20:27:14 -0300

    Refactor MainProvider type definition
    Moved `MainProvidersProps` type definition to a separate `types.ts` file for better code organization and maintainability. This change enhances the clarity of the `MainProvider` component by decluttering its implementation file.

--- src/components/providers/mainProvider/index.tsx
+++ src/components/providers/mainProvider/index.tsx
@@ -1,7 +1,5 @@
 import type { ReactNode } from 'react'
 
-import type { MainProvidersProps } from './types'
-
 export const MainProvider = ({ children }: MainProvidersProps): ReactNode => {
   return <>{children}</>
 }
